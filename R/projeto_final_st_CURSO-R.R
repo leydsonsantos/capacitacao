8cd2b98e89bb033cb3922c7dceaf45421f01ba0e
Leydson_Santos <consult.leydson@gmail.com>
2022-12-04 22:43:58 -0300

    atualizado carregamenti.

--- R/projeto_final_st_CURSO-R.R
+++ R/projeto_final_st_CURSO-R.R
@@ -1,9 +1,23 @@
+#Geracao mensal de eletricidade nos EUA.
 
-# Geração mensal de eletricidade nos EUA (em milhões de quilowatts-hora)
-# de todos os tipos: gás natural.
+#Administracao de Informacao de Energia dos EUA.
+
+#Consumo de combustiveis para geracao de eletricidade:
+
+#Consumo de Gas Natural para Geracao de Eletricidade,
+#Setor de Energia Eletrica. Em bilhões de pes cubicos.
 
 
 
 # carregar base de dados --------------------------------------------------
 
+setwd("C:/Users/Leydson/Desktop/DSA/learn")
 
+files <- dir(pattern = "NG_2022.csv$")
+
+dados <- as.tibble(files %>%
+  map_dfr(fread))
+
+dados$Gas_Consumption <- str_replace(dados$Gas_Consumption,",",".")
+
+dados$Gas_Consumption <- as.numeric(dados$Gas_Consumption)
